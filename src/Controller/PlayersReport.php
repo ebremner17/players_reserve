c5e82ec8c1f7c4a3391fc1396d02a0208926aea5
ebremner <ebremner@uwaterloo.ca>
2022-12-16 10:38:57 -0500

    Adding reports

---
+++ src/Controller/PlayersReport.php
@@ -0,0 +1,391 @@
+<?php
+
+namespace Drupal\players_reserve\Controller;
+
+use DateTime;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Url;
+use Drupal\players_reserve\Service\PlayersService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Report for Players Inc..
+ */
+class PlayersReport extends ControllerBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The Messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The players service.
+   *
+   * @var \Drupal\players_reserve\Service\PlayersService
+   */
+  protected $playersService;
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTyperManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   * @param \Drupal\players_reserve\Service $playersService
+   *   The players service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *   The current request stack.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entityTypeManager,
+    MessengerInterface $messenger,
+    PlayersService $playersService,
+    RequestStack $requestStack,
+    Connection $database
+  ) {
+
+    $this->entityTypeManager = $entityTypeManager;
+    $this->messenger = $messenger;
+    $this->playersService = $playersService;
+    $this->requestStack = $requestStack;
+    $this->database = $database;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('messenger'),
+      $container->get('players_reserve.players_service'),
+      $container->get('request_stack'),
+      $container->get('database')
+    );
+  }
+
+  /**
+   * Returns a page for players report.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function report(): array {
+
+    // Get the parameters from the URL.
+    $parameters = $this->requestStack->getCurrentRequest()->query->all();
+
+    // If there are no parameters, redirect back to the
+    // reports page.
+    if (empty($parameters)) {
+     $this->redirectToReports();
+    }
+
+    // Get the correct report based on the report type.
+    switch ($parameters['report_type']) {
+
+      // The single game report.
+      case 'single_game_report':
+
+        // Ensure that we have the correct parameters.
+        $this->checkForParameters($parameters, ['date']);
+
+        // Get the report.
+        $report_info['reservations'] = $this->getSingleGameReport($parameters);
+
+        // Set the title.
+        $report_info['title'] = 'Single Game Report';
+
+        break;
+
+      case 'multiple_game_report':
+
+        // Ensure that we have the correct parameters.
+        $this->checkForParameters($parameters, ['start_date', 'end_date', 'status']);
+
+        // Get the report.
+        $report_info['reservations'] = $this->getMultipleGameReport($parameters);
+
+        // Set the title.
+        $report_info['title'] = 'Multiple Game Report';
+
+        break;
+    }
+
+    // Set the subtitle.
+    $report_info['sub_title'] = 'Generated on ' . date('l F j, Y h:i:s A', strtotime('now'));
+
+    // Set the render array.
+    return [
+      '#theme' => 'players_report',
+      '#report_info' => $report_info,
+    ];
+  }
+
+  /**
+   * Function to check for the correct parameters.
+   *
+   * @param array $parameters
+   *   The array of parameters sent.
+   * @param array $params_to_check
+   *   The parameters to check for.
+   */
+  public function checkForParameters(array $parameters, array $params_to_check): void {
+
+    // If there are no parameters that intersect, send back
+    // to the select reports page.
+    if (empty(array_intersect(array_keys($parameters), $params_to_check))) {
+      $this->redirectToReports();
+      exit;
+    }
+  }
+
+  /**
+   * Function to redirect back to the reports page.
+   */
+  public function redirectToReports(): void {
+
+    $url = Url::fromUri('internal:/admin/reserve/reports')->toString();
+    $response = new RedirectResponse($url);
+    $response->send();
+  }
+  /**
+   * Function to get the single game report.
+   *
+   * @param string $date
+   *   The date of the game.
+   *
+   * @return array
+   *   The array of data about the game.
+   */
+  public function getSingleGameReport(array $parameters): array {
+
+    // Load the node based on the date selection.
+    $node = current($this->entityTypeManager->getStorage('node')->loadByProperties(['title' => $parameters['date']]));
+
+    // If there is no node, redirect back to the select reports page,
+    // with a message.
+    if (!$node) {
+      $display_date = date('l F j, Y', strtotime($parameters['date']));
+      $this->messenger->addError('There is no game data for ' . $display_date . '.');
+      $this->redirectToReports();
+    }
+
+    $reservations[] = $this->getReservationTable($node->id(), $parameters['date'], $parameters);
+
+    return $reservations;
+  }
+
+  /**
+   * Function to get the multiple game report.
+   *
+   * @param array $parameters
+   *   Array of parameters.
+   *
+   * @return array
+   *   Array of reservations.
+   */
+  public function getMultipleGameReport(array $parameters): array {
+
+    $earlier = new DateTime($parameters['start_date']);
+    $later = new DateTime($parameters['end_date']);
+    $dates_diff = $later->diff($earlier)->format("%a");
+
+    for ($i = 0; $i <= $dates_diff; $i++) {
+      $dates[] = date('Y-m-d', strtotime($parameters['start_date'] . ' +' . $i . ' day'));
+    }
+
+    foreach ($dates as $date) {
+
+      // Load the node based on the date selection.
+      $node = current($this->entityTypeManager->getStorage('node')->loadByProperties(['title' => $date]));
+
+      // If there is no node, redirect back to the select reports page,
+      // with a message.
+      if (!$node) {
+        $display_date = date('l F j, Y', strtotime($parameters['date']));
+        // The table for the list.
+        $reservations[] = [
+          'date' => date('l F j, Y', strtotime($parameters['date'])),
+          'open' => FALSE,
+          'table' => [
+            '#type' => 'markup',
+            '#markup' => 'There is no game data for ' . $display_date . '.',
+          ],
+        ];
+      }
+      else {
+
+        $reservations[] = $this->getReservationTable($node->id(), $date, $parameters, FALSE);
+      }
+    }
+
+    return $reservations;//3
+  }
+
+  /**
+   * Function to get the table of reservations.
+   *
+   * @param int $nid
+   *   The node id.
+   * @param string $date
+   *   The date to get the game data.
+   * @param array $parameters
+   *   The array of parameters.
+   * @param bool $open_flag
+   *   Flag to show the details as open or closed.
+   *
+   * @return array
+   *   The array for reservations.
+   */
+  public function getReservationTable(int $nid, string $date, array $parameters, bool $open_flag = TRUE): array {
+
+    // The header for the table.
+    $header = [
+      ['data' => t('Last Name'), 'field' => 'last_name', 'sort' => 'asc'],
+      ['data' => $this->t('First Name'), 'field' => 'first_name', 'sort' => 'asc'],
+      ['data' => $this->t('Game'), 'field' => 'game', 'sort' => 'asc'],
+      ['data' => t('Status')],
+    ];
+
+    // Get the reservations for the game.
+    $results = $this->getReservationsByGame($nid, $parameters);
+
+    if (empty($results)) {
+      return [
+        'date' => date('l F j, Y', strtotime($date)),
+        'count' => NULL,
+        'open' => $open_flag,
+        'table' => [
+          '#type' => 'markup',
+          '#markup' => 'There is no game data for ' . date('l F j, Y', strtotime($date)) . '.',
+        ],
+      ];
+    }
+
+    // Step through each result and get the rows.
+    foreach ($results as $result) {
+
+      // Add in the correct thing for status.
+      if ($parameters['status'] == 'both') {
+        $status = $result->seated ? 'Seated' : 'Removed';
+      }
+      else {
+        if ($parameters['status'] == 'seated') {
+          $status = 'Seated';
+        }
+        else {
+          $status = 'Removed';
+        }
+      }
+
+      // Set the rows.
+      $rows[] = [
+        'data' => [
+          'first_name' => $result->last_name,
+          'last_name' => $result->first_name,
+          'game' => $result->game_type,
+          'status' => $status,
+        ],
+      ];
+    }
+
+    // The table for the list.
+    return [
+      'date' => date('l F j, Y', strtotime($date)),
+      'count' => count($results),
+      'open' => $open_flag,
+      'table' => [
+        '#type' => 'table',
+        '#title' => 'Game data',
+        '#header' => $header,
+        '#rows' => $rows,
+      ],
+    ];
+  }
+
+  /**
+   * Function to get the reservations by the game.
+   *
+   * @param int $nid
+   *   The node id.
+   * @param array $parameters
+   *   The parameters for the page.
+   *
+   * @return array
+   *   Array of reservations.
+   */
+  public function getReservationsByGame(int $nid, array $parameters): array {
+
+    // The query to get the reservations for the game.
+    $query = $this->database->select('players_reserve', 'pr')
+      ->fields('pr', ['reserve_id', 'first_name', 'last_name', 'game_type', 'seated', 'removed'])
+      ->condition('pr.nid', $nid);
+
+    // If the status parameters is seated, add the condition.
+    if ($parameters['status'] == 'seated') {
+      $query->condition('pr.seated', 1);
+    }
+
+    // If the status parameter is removed, add the condition.
+    if ($parameters['status'] == 'removed') {
+      $query->condition('pr.removed', 1);
+    }
+
+    // If the order is set, add to the query.
+    if (isset($parameters['order'])) {
+
+      // If the order is by game, set to proper field.
+      // If not just use the order parameter.
+      if ($parameters['order'] == 'Game') {
+        $query->orderBy('game_type', $parameters['sort']);
+        $query->orderBy('last_name', 'asc');
+      }
+      else {
+        $order_by_name = strtolower($parameters['order']);
+        $order_by_name = str_replace(' ', '_', $order_by_name);
+        $query->orderBy($order_by_name, $parameters['sort']);
+      }
+    }
+    else {
+
+      // By default sort by last name.
+      $query->orderBy('last_name', 'asc');
+    }
+
+    // Return the results.
+    return $query->execute()->fetchAll();
+  }
+
+}
---
+++ src/Form/PlayersSelectReportForm.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\players_reserve\Form\PlayersAddReserveForm.
+ */
+namespace Drupal\players_reserve\Form;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+
+class PlayersSelectReportForm extends FormBase {
+
+  /**
+   * The Messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * @param \Drupal\players_reserve\Service\PlayersService $playersService
+   *  The players service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   */
+  public function __construct(
+    MessengerInterface $messenger,
+    EntityTypeManagerInterface $entityTypeManager
+  ) {
+
+    $this->messenger = $messenger;
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+
+    // Instantiates this form class.
+    return new static(
+    // Load the service required to construct this class.
+      $container->get('messenger'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'players_select_report_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $date = NULL) {
+
+    $form['report_type_wrapper'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Report Type'),
+    ];
+
+    $form['report_type_wrapper']['report_type'] = [
+      '#type' => 'select',
+      '#title' => 'Report type',
+      '#options' => [
+        '' => 'Select',
+        'single_game_report' => 'Single game report',
+        'multiple_game_report' => 'Multiple game report',
+      ],
+      '#required' => TRUE,
+    ];
+
+    $form['report_details_wrapper'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Report details'),
+      '#states' => [
+        'invisible' => [
+          ':input[name="report_type"]' => ['value' => ''],
+        ],
+      ],
+    ];
+
+    $form['report_details_wrapper']['game_date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Game date'),
+      '#states' => [
+        'visible' => [
+          ':input[name="report_type"]' => ['value' => 'single_game_report'],
+        ],
+        'required' => [
+          ':input[name="report_type"]' => ['value' => 'single_game_report'],
+        ],
+      ],
+    ];
+
+    $form['report_details_wrapper']['status'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Status'),
+      '#options' => [
+        'both' => 'Both',
+        'removed' => 'Removed',
+        'seated' => 'Seated',
+      ],
+      '#default_value' => 'both',
+      '#states' => [
+        'visible' => [
+          ':input[name="report_type"]' => [
+            ['value' => 'single_game_report'],
+            ['value' => 'multiple_game_report']
+          ],
+        ],
+        'required' => [
+          ':input[name="report_type"]' => [
+            ['value' => 'single_game_report'],
+            ['value' => 'multiple_game_report']
+          ],
+        ],
+      ],
+    ];
+
+    $form['report_details_wrapper']['game_start_date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Game start date'),
+      '#states' => [
+        'visible' => [
+          ':input[name="report_type"]' => ['value' => 'multiple_game_report'],
+        ],
+        'required' => [
+          ':input[name="report_type"]' => ['value' => 'multiple_game_report'],
+        ],
+      ],
+    ];
+
+    $form['report_details_wrapper']['game_end_date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Game end date'),
+      '#states' => [
+        'visible' => [
+          ':input[name="report_type"]' => ['value' => 'multiple_game_report'],
+        ],
+        'required' => [
+          ':input[name="report_type"]' => ['value' => 'multiple_game_report'],
+        ],
+      ],
+    ];
+
+    // Submit buttons.
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+      '#button_type' => 'primary',
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    // Get the values from the form state.
+    $values = $form_state->getValues();
+
+    // If we are on a multiple game report, ensure that the
+    // start date is greater than the end date.
+    if ($values['report_type'] == 'multiple_game_report') {
+
+      // If the start date is less than the end date,
+      // set the form error.
+      if ($values['game_start_date'] > $values['game_end_date']) {
+        $form_state->setError($form['report_details_wrapper']['game_start_date'], 'The start date must be before the end date.');
+      }
+    }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Get the values from the form state.
+    $values = $form_state->getValues();
+
+    // url to redirect
+    $path = '/admin/reserve/report';
+
+    // Set the query parameters for the selected report.
+    switch ($values['report_type']) {
+
+      case 'single_game_report':
+        $path_param = [
+          'report_type' => $values['report_type'],
+          'date' => $values['game_date'],
+          'status' => $values['status'],
+        ];
+        break;
+
+      case 'multiple_game_report':
+        $path_param = [
+          'report_type' => $values['report_type'],
+          'start_date' => $values['game_start_date'],
+          'end_date' => $values['game_end_date'],
+          'status' => $values['status'],
+        ];
+    }
+
+    // use below if you have to redirect on your known url
+    $url = Url::fromUserInput($path, ['query' => $path_param]);
+
+    $form_state->setRedirectUrl($url);
+  }
+
+}
