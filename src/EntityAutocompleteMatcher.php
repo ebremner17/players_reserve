d6912f692e1a67d9d3a09f6d8c6b312940d00447
ebremner <ebremner@uwaterloo.ca>
2022-12-15 22:27:49 -0500

    Fixing the users autocomplete

--- src/EntityAutocompleteMatcher.php
+++ src/EntityAutocompleteMatcher.php
@@ -42,10 +42,15 @@ class EntityAutocompleteMatcher extends \Drupal\Core\Entity\EntityAutocompleteMa
    */
   public function getMatches($target_type, $selection_handler, $selection_settings, $string = '') {
 
+    // If not user, do not modify.
     if ($target_type !== 'user') {
       return;
     }
 
+    // Get the escaped value to search for.
+    $escaped = $this->database->escapeLike($string);
+
+    // Get the query for the users.
     $query = $this->database->select('users', 'u');
     $query->join('users_field_data', 'ufd', 'u.uid = ufd.uid');
     $query->join('user__field_user_first_name', 'fn', 'u.uid = fn.entity_id');
@@ -55,15 +60,24 @@ class EntityAutocompleteMatcher extends \Drupal\Core\Entity\EntityAutocompleteMa
       ->fields('ln', ['field_user_last_name_value'])
       ->fields('ufd', ['mail']);
 
+    // Add the or conditions for first and last name.
     $orGroup = $query->orConditionGroup()
-      ->condition('fn.field_user_first_name_value', '%' . $string . '%', 'LIKE')
-      ->condition('ln.field_user_last_name_value', '%' . $string . '%', 'LIKE');
+      ->condition('fn.field_user_first_name_value', '%' . $escaped . '%', 'LIKE')
+      ->condition('ln.field_user_last_name_value', '%' . $escaped . '%', 'LIKE');
 
+    // Add the or condition to the query.
     $query->condition($orGroup);
 
+    // Set the order by.
+    $query->orderBy('ln.field_user_last_name_value', 'ASC');
+
+    // Perform the query.
     $users = $query->execute()->fetchAll();
 
+    // If there are users, modify the output.
     if ($users) {
+
+      // Step through each user and set the output.
       foreach ($users as $user) {
         $label = $user->field_user_first_name_value;
         $label .= ' ';
@@ -71,7 +85,6 @@ class EntityAutocompleteMatcher extends \Drupal\Core\Entity\EntityAutocompleteMa
         $label .= ' (';
         $label .= $user->uid;
         $label .= ')';
-        $label .= ' [' . $user->mail . ']';
         $matches[] = ['value' => $label, 'label' => $label];
       }
     }
